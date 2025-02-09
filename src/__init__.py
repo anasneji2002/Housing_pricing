61338468118528d83d19f0194fbd02e6b1bfd55d
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-02-09 21:09:47 +0100

    add model training and data extraction logic

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,9 @@
 # src/__init__.py
 from .data import load_data
-from .features import build_features, select_features
-from .models import train_model, evaluate_model
-from .visualization import plot_results
+from .models import train_model
+
 
 __all__ = [
     "load_data",
-    "build_features",
-    "select_features",
-    "train_model",
-    "evaluate_model",
-    "plot_results",
+    "train_model"
 ]
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 # src/models/__init__.py
 from .train_model import train_model
-from .evaluate_model import evaluate_model
 
-__all__ = ["train_model", "evaluate_model"]
+
+__all__ = ["train_model"]
