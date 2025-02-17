82b5a208142e5bf00a815e486098df5e8d0caf61
Mohamed Anas Neji <mohamedanas.neji@medius.com>
2025-02-17 12:22:08 +0100

    fix model

--- src/backend/routes/predict.py
+++ src/backend/routes/predict.py
@@ -1,10 +1,12 @@
 import joblib
 import os
+from sklearn.preprocessing import StandardScaler
 
 from backend.schemas import PredictionRequest, CITY_MAPPING
 
 # Load trained model (update path if needed)
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "models", "model.pkl")
+SCALER_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "models", "scaler.pkl")
 
 try:
     model = joblib.load(MODEL_PATH)
@@ -12,14 +14,24 @@ except Exception as e:
     print(f"⚠️ Model not found: {e}")
     model = None
 
+try:
+    scaler = joblib.load(SCALER_PATH)
+except Exception as e:
+    print(f"⚠️ scaler not found: {e}")
+    scaler = None
+
 
 def predict_price(request: PredictionRequest):
     """Predict house price based on input features."""
-    if model is None:
-        raise ValueError("Model is not loaded. Ensure 'model.pkl' exists.")
+    if model is None or scaler is None:
+        raise ValueError("Model or scaler is not loaded. Ensure 'model.pkl' exists.")
+
+    print(request)
 
     # Convert city name to corresponding ID
     city_id = CITY_MAPPING.get(request.city, 0)  # Default to 0 if not found
+
+    print(f'city_id{city_id}')
 
     features = [
         request.surface,
@@ -35,5 +47,7 @@ def predict_price(request: PredictionRequest):
         request.ascenseur,
     ]
 
-    prediction = model.predict([features])[0]
-    return round(prediction, 2)
+    features_after_normalization = scaler.transform([features])
+    prediction = model.predict(features_after_normalization)
+    print(prediction)
+    return round(prediction[0], 2)
